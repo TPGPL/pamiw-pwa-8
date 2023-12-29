a7b5c499a890e09db6028ec2dc16f67b55de3895
TPGPL <TPGPL@users.noreply.github.com>
2023-12-29 20:02:38 +0100

    Add app configuration

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PamiwPwa;
+using PamiwPwa.Security;
+using PamiwShared.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl")) });
+builder.Services.AddSingleton<IAuthorService, AuthorService>();
+builder.Services.AddSingleton<IPublisherService, PublisherService>();
+builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddSingleton<IAuthService, AuthService>();
+
+builder.Services.AddSingleton<AuthState>();
 
 await builder.Build().RunAsync();
---
+++ Security/AuthState.cs
@@ -0,0 +1,22 @@
+﻿namespace PamiwPwa.Security;
+
+public class AuthState
+{
+    public bool IsAuthenticated { get; private set; }
+    public string Username { get; private set; } = string.Empty;
+    public event Action OnChange;
+
+    public void SetAuth(bool auth, string username)
+    {
+        Username = username;
+        SetAuth(auth);
+    }
+
+    public void SetAuth(bool auth)
+    {
+        IsAuthenticated = auth;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
